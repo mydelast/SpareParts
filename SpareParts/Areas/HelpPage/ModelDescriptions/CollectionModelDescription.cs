90ff75076f0cf665ab9aa3aa984d625db4cc6455
Mydel-Ar Asturiano (USG2) <Mydel_Asturiano@utacgroup.com>
2018-05-22 00:33:03 +0800

    First commit

---
+++ SpareParts/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SpareParts.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SpareParts/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SpareParts.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
